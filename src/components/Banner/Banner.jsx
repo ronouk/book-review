782c175dbcaafd6c7b4a4ec516ee42062dcf6d37
ronouk <shubro42@yahoo.com>
2026-03-26 14:46:50 +0400

    minor fix

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,16 +1,17 @@
+import { NavLink } from "react-router";
 
 const Banner = () => {
     return (
 
-                <div className="hero mb-12 lg:mb-24">
-                    <div className="w-10/12 bg-gray-200 text-black flex flex-col justify-between items-center lg:flex-row-reverse p-12 rounded-xl">
-                        <img src="/banner.png" className="max-w-md lg:max-w-md xl:max-w-xl py-12" />
-                        <div className="text-center lg:text-left space-y-8">
-                            <h1 className="text-3xl md:text-4xl lg:text-5xl font-extrabold lg:leading-tight heading">Books to freshen up<br />your bookshelf</h1>
-                            <button className="btn shadow-none border-none bg-[#23BE0A] text-white">View The List</button>
-                        </div>
-                    </div>
+        <div className="hero mb-12 lg:mb-24">
+            <div className="w-10/12 bg-gray-200 text-black flex flex-col justify-between items-center lg:flex-row-reverse p-12 rounded-xl">
+                <img src="/banner.png" className="max-w-md lg:max-w-md xl:max-w-xl py-12" />
+                <div className="text-center lg:text-left space-y-8">
+                    <h1 className="text-3xl md:text-4xl lg:text-5xl font-extrabold lg:leading-tight heading">Books to freshen up<br />your bookshelf</h1>
+                    <NavLink to='/allbooks'><button className="btn shadow-none border-none bg-[#23BE0A] text-white">View The List</button></NavLink>
                 </div>
+            </div>
+        </div>
 
     );
 };
